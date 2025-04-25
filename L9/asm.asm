b5e53089f3e49b4074fa5b52c1d3bcddf78d1435
Ryan King <ryan.king157@gmail.com>
2025-04-25 15:59:04 -0700

    add a macro

--- L9/asm.asm
+++ L9/asm.asm
@@ -2,9 +2,20 @@
 ; Windows ABI rcx, rdx, r8, and r9
 ; Floating Point
 ; L/W ABI: xmm0, xmm1,...
-;
-; extern "C" Int64 Cc1_(int8_t a, int16_t b, int32_t c, int64_t d, int8_t e, int16_t f,
-; int32_t g, int64_t h);
+
+; Define macros for function prologue and epilogue
+   %macro FUNCTION_PROLOGUE 1 ; Parameter is the stack space to allocate
+    push rbp ; save caller's rbp reg
+    mov rbp, rsp ; update frame pointer
+    sub rsp, %1 ; allocate stack space for local variables
+   %endmacro
+
+   %macro FUNCTION_EPILOGUE 0 ; No parameters needed
+    mov rsp, rbp ; restore stack pointer
+    pop rbp ; restore base pointer
+    ret ; return to caller
+   %endmacro
+
 section .data
    %define RBP_OFFSET 16 ; Adjusted this value
 
@@ -13,31 +24,29 @@ section .text
 
 Cc1_:
     ; Function prologue
-    push rbp ; save caller's rbp reg
-    mov rbp, rsp ; update frame pointer
-    sub rsp, 16 ; allocate stack space for local variables
+    FUNCTION_PROLOGUE 16 ; Allocate 16 bytes of stack space
 
     ; Process first 6 args which are in registers in Linux ABI
     movsx rax, dil ; a (8-bit)
-    movsx rcx, si ; b (16-bit)
+    movsx rsi, si ; b (16-bit)
     movsxd rdx, edx ; c (32-bit)
     ; d is already in rcx as 64-bit
-    movsx r10, r8b ; e (8-bit)
-    movsx r11, r9w ; f (16-bit)
+    movsx r8, r8b ; e (8-bit)
+    movsx r9, r9w ; f (16-bit)
 
     ; Calculate a+b+c+d
-    add rax, rcx ; a+b
-    add rdx, r8 ; c+d
+    add rax, rsi ; a+b
+    add rdx, rcx ; c+d
     add rax, rdx ; a+b+c+d
     mov [rbp-8], rax ; store this temporarily
 
     ; Get g and h from stack (7th and 8th parameters)
-    movsxd r8, dword [rbp+16]  ; g (7th param) - 32-bit
-    mov r9, [rbp+24]           ; h (8th param) - 64-bit
+    movsxd r10, dword [rbp+RBP_OFFSET]    ; g (7th param) - 32-bit
+    mov r11, [rbp+RBP_OFFSET+8]           ; h (8th param) - 64-bit
 
     ; Calculate e+f+g+h
-    add r10, r11 ; e+f
-    add r8, r9 ; g+h
+    add r8, r9 ; e+f
+    add r10, r11 ; g+h
     add r10, r8 ; e+f+g+h
 
     ; Calculate final result
@@ -45,8 +54,6 @@ Cc1_:
     add rax, r10 ; (a+b+c+d) + (e+f+g+h)
 
     ; Function epilogue
-    mov rsp, rbp ; restore stack pointer
-    pop rbp ; restore base pointer
-    ret
+    FUNCTION_EPILOGUE
 
 section .note.GNU-stack
